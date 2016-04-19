4026262dc8ba7016fe9475651351807e03484ebb
Oliver Finn Madsen <mail@ofmadsen.com>
2016-04-19 15:15:34 +0200

    Add default entity class

---
+++ src/Livid/Entity.php
@@ -0,0 +1,16 @@
+<?php
+namespace Livid;
+
+/**
+ * Default entity class.
+ *
+ * Instances of this class is used by the Query object when an entity is unspecified.
+ *
+ * @see \Livid\Query::get
+ * @see \Livid\Query::all
+ *
+ * @author Oliver Finn Madsen <mail@ofmadsen.com>
+ */
+class Entity
+{
+}
